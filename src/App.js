9aef0871bb39fb6d068e3d11f048c22f1cd2e6a9
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-08-12 23:22:36 +0600

    initializing project, deleted no - need files, boilerplate

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <h1>Ogogo Academy - Start</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
