2b7cb843041ab5d5bb5e78e4a325d4ae7eb3b358
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-08-25 15:07:12 +0600

    add content to hero section, made refactoring, made 'btn' class component

--- src/landing/components/header/authBtn.jsx
+++ src/landing/components/header/authBtn.jsx
@@ -4,11 +4,11 @@ import UserIcon from '../../assets/svg/user-icon.svg';
 function AuthBtns() {
   return (
     <div className='header__auth-btns auth-btns'>
-      <button className='auth-btns__log-in'>
+      <button className='btn auth-btns__log-in btn-light-blue'>
         <img src={UserIcon} alt='' />
         Войти
       </button>
-      <button className='auth-btns__sign-up'>
+      <button className='btn auth-btns__sign-up btn-white'>
         <img src={UserIcon} alt='' />
         Регистрация
       </button>
