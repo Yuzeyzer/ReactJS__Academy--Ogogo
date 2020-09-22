a879a0310b502c79ebe63b706d45f903e02ab0a8
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-09-22 18:06:47 +0600

    created const.jsx to make code more readiable

---
+++ src/landing/sections/destinations/const.jsx
@@ -0,0 +1,33 @@
+import iconUxUi from '../../assets/svg/ux-ui.svg';
+import iconPython from '../../assets/svg/python.svg';
+import icon3d from '../../assets/svg/3d.svg';
+import iconFrontend from '../../assets/svg/frontend.svg';
+import iconJava from '../../assets/svg/java.svg';
+
+export const item = [
+  {
+    icon: `${iconUxUi}`,
+    title: 'UX/UI',
+    format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
+  },
+  {
+    icon: `${iconPython}`,
+    title: 'Python',
+    format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
+  },
+  {
+    icon: `${icon3d}`,
+    title: '3D',
+    format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
+  },
+  {
+    icon: `${iconFrontend}`,
+    title: 'Front - End',
+    format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
+  },
+  {
+    icon: `${iconJava}`,
+    title: 'Java',
+    format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
+  },
+];
--- src/landing/sections/destinations/item.jsx
+++ src/landing/sections/destinations/item.jsx
@@ -1,42 +1,10 @@
 import React from 'react';
-import iconUxUi from '../../assets/svg/ux-ui.svg';
-import iconPython from '../../assets/svg/python.svg';
-import icon3d from '../../assets/svg/3d.svg';
-import iconFrontend from '../../assets/svg/frontend.svg';
-import iconJava from '../../assets/svg/java.svg';
-
+import { item } from './const';
 function Item() {
-  const item = [
-    {
-      icon: `${iconUxUi}`,
-      title: 'UX/UI',
-      format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
-    },
-    {
-      icon: `${iconPython}`,
-      title: 'Python',
-      format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
-    },
-    {
-      icon: `${icon3d}`,
-      title: '3D',
-      format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
-    },
-    {
-      icon: `${iconFrontend}`,
-      title: 'Front - End',
-      format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
-    },
-    {
-      icon: `${iconJava}`,
-      title: 'Java',
-      format: 'Длительность - 3 месяца Формат - 5 раз в неделю',
-		},
-  ];
   return (
     <React.Fragment>
       {item &&
-        item.map(item => {
+        item.map((item) => {
           return (
             <div className='destinations__column' key={item.title}>
               <div className='destinations__item'>
