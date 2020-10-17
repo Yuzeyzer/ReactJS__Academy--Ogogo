e4bc6feabf7722e26528f8df739ae7226b0a41fb
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-10-17 18:32:40 +0600

    new section, project name change + scss

--- src/landing/sections/index.js
+++ src/landing/sections/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import Hero from './hero';
 import Destinations from './destinations';
-import Courses from './courses'
+import Courses from './courses';
+import Mentors from './mentors';
 
 function Sections() {
 	return (
@@ -9,6 +10,7 @@ function Sections() {
 			<Hero />
 			<Destinations />
 			<Courses />
+			<Mentors />
 		</main>
 	)
 }
---
+++ src/landing/sections/mentors/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Layout from './layout';
+import { item } from './const';
+import './styles.scss';
+
+const Mentors = () => {
+  return <Layout item={item}/>;
+};
+
+export default Mentors;
