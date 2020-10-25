4498252767d95c56c2648597333bf78ad1208816
Yuzeyzer <kurbanof.atabek@gmail.com>
2020-10-25 06:12:26 +0600

    adding new features, photo generator

---
+++ src/landing/credentials.js
@@ -0,0 +1,2 @@
+export const ACCESS_KEY = `vPsDPWwmEM9h8V7OFKv9iqxyMO6pvcdPS7QXsAY4cv4`;
+export const API_URL = `https://api.unsplash.com`;
--- src/landing/sections/hero/index.js
+++ src/landing/sections/hero/index.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import HeroContent from './HeroContent';
-import rightBg from '../../assets/png/right-img.png';
+import Images from './images';
 import './styles.scss';
 
 function Hero() {
   return (
     <section className='hero'>
+
       <div className='container flex-space-between'>
         <HeroContent />
-        <div className='hero__block-img'>
-          <img className='hero__img' src={rightBg} alt='' />
+        <div className='hero__interesting interesting'>
+          <h5 className='ineteresting__title'>Это интересно</h5>
+          <p className='interesting__description'>
+            Попробуй обновить сайт и ты увидишь что при каждом обновлении страницы генерируются
+            разные картинки. Это базовый пример работы с API с точки зрения фронтенда.
+          </p>
+          <p className='interesting__description'>
+            А если тебе лень обновлять страницу, то можешь просто нажать на кнопочку которая
+            перезапустит функцию. В программировании же это называется повторный вызов функции.
+          </p>
+          <button className='btn btn-pink'>Вызов функции</button>
+          <Images className={'interesting__images'}/>
         </div>
       </div>
     </section>
